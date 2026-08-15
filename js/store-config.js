59c517483603deed7150bec8dbde3ea9d952d238
tool-bucket <vlogsnitinsaini@gmail.com>
2026-08-15 11:10:59 +0530

    Add files via upload

---
+++ js/store-config.js
@@ -0,0 +1,8 @@
+
+/* FreshJaipur business settings.
+   IMPORTANT: put your WhatsApp Business number here.
+   India example: 919876543210 (without +, spaces or dashes).
+*/
+window.FRESHJAIPUR_CONFIG = {
+    whatsappNumber: ""
+};
