6b71e95757a958ff67b1036017cb95ea9cb79cc4
tool-bucket <vlogsnitinsaini@gmail.com>
2026-08-15 11:09:48 +0530

    Delete store-config.js

--- store-config.js
+++
@@ -1,8 +0,0 @@
-
-/* FreshJaipur business settings.
-   IMPORTANT: put your WhatsApp Business number here.
-   India example: 919876543210 (without +, spaces or dashes).
-*/
-window.FRESHJAIPUR_CONFIG = {
-    whatsappNumber: ""
-};
